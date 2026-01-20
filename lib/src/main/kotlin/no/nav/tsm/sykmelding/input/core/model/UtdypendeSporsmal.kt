252f74ac2f6c8258638b3abd45e518d183b841d5
Halvor Grizzly Bjørn <halvor.grizzly.bjorn@nav.no>
2026-01-20 11:05:12 +0100

    feat: add support for 'utdypende spørsmål' in week 17 and 39
    Co-authored-by: Leo-Andreas Ervik <leo-andreas.ervik@nav.no>
    Co-authored-by: Andreas Sagen Aspaas <Andreas.Sagen.Aspaas@nav.no>

--- lib/src/main/kotlin/no/nav/tsm/sykmelding/input/core/model/UtdypendeSporsmal.kt
+++ lib/src/main/kotlin/no/nav/tsm/sykmelding/input/core/model/UtdypendeSporsmal.kt
@@ -4,10 +4,15 @@ enum class Sporsmalstype {
     UTFORDRINGER_MED_GRADERT_ARBEID,
     MEDISINSK_OPPSUMMERING,
     HENSYN_PA_ARBEIDSPLASSEN,
+    BEHANDLING_OG_FREMTIDIG_ARBEID,
+    UAVKLARTE_FORHOLD,
+    FORVENTET_HELSETILSTAND_UTVIKLING,
+    MEDISINSKE_HENSYN,
 }
 
 data class UtdypendeSporsmal(
     val svar: String,
     val type: Sporsmalstype,
     val skjermetForArbeidsgiver: Boolean = true,
+    val sporsmal: String?,
 )
